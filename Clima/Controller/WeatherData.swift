b63ca07b80675a66bc5c0cacc159f87d2f2fab72
murrdis <dias.murzagaliyev@nu.edu.kz>
2022-12-24 02:28:11 +0600

    clima

---
+++ Clima/Controller/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Алмаз Кусаинов on 15.10.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+    let feels_like: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+}
