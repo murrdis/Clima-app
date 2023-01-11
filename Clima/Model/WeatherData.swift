e350250fa514997e974efe58f0dc5831ea5ea583
murrdis <121186235+murrdis@users.noreply.github.com>
2023-01-11 16:47:13 +0600

    Add files via upload

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Диас Мурзагалиев on 06.01.2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+    let id: Int
+}
